3bd6fcef0dbfef43c622c5c1629ba2a9c5069a4d
khanzaifa37 <33030790+khanzaifa37@users.noreply.github.com>
2018-12-01 00:57:17 +0530

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -1,3 +1,6 @@
+
+#include <iostream>
+using namespace std;
 void swap(int a,int b)
 {
   int c;
@@ -5,3 +8,12 @@ void swap(int a,int b)
   a=b;
   b=c;
  }
+
+int main()
+{
+  int a,b;
+  cin>>a>>b;
+  swap(a,b);
+  cout <<a<<b;
+  return 0;
+}
